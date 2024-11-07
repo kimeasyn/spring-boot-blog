f5d0ce7f17e09fa2c40ce9c1be7b588661a048b1
heejin <kimeasyn@gmail.com>
2024-11-07 15:22:25 +0900

    feat: article list template

---
+++ spring-blog/src/main/java/me/bootblog/controller/BlogViewController.java
@@ -0,0 +1,26 @@
+package me.bootblog.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.bootblog.dto.ArticleListViewResponse;
+import me.bootblog.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
